5339aab0a82ca68495f702f513c459a7c61dd529
hero777k <kobac5hiro726@gmail.com>
2024-10-05 00:31:59 +0900

    Fix Vite build issue

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+    },
 });
