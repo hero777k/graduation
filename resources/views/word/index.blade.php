842bdcaf416f424dfb0be830b1ef7d5f9414d0b0
hero777k <kobac5hiro726@gmail.com>
2024-10-07 22:31:40 +0900

    フラッシュメッセージ設置２

--- resources/views/word/index.blade.php
+++ resources/views/word/index.blade.php
@@ -8,6 +8,28 @@
 
 @section('content')
 
+@if(session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
         <!-- 絞り込み・フリーワード検索フォーム -->
         <form method="GET" action="{{ route('word.index') }}" class="mb-3">
             <div class="input-group-append">
