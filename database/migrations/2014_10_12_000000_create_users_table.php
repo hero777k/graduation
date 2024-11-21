86bb830d6f29175821dbd55c55865b9a40531057
hero777k <kobac5hiro726@gmail.com>
2024-11-21 12:18:11 +0900

    メッセージを日本語化

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',100)->comment("名前");
+            $table->string('email',255)->unique()->comment("メールアドレス");
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',255)->comment("パスワード");
             $table->rememberToken();
+            $table->integer("role")->length(2)->default(2)->comment("1:管理者、2=一般");
             $table->timestamps();
         });
     }
